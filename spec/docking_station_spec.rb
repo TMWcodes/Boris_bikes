7055fc2c9cb81db59081fca366fdf8fe87cb5993
Tyrone Williams <tyrone@Tyrones-MacBook-Air.local>
2022-01-10 13:28:04 +0000

    first test passed (respond_to)

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,2 +1,8 @@
+require 'docking_station'
+
 describe Dockingstation do
+    # it 'responds to release_bike' do
+    #expect(Dockingstation).to respond_to :release_bike
+    #end
+    it {is_expected.to respond_to :release_bike}
 end
